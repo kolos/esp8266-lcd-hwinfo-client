6bb6b67667d85adda48999c3fe46806cd77fa322
kolos <kolos@users.noreply.github.com>
2019-04-13 11:49:34 +0200

    init

---
+++ constants.example.h
@@ -0,0 +1,16 @@
+#define SERIAL_BAUD_RATE 115200
+
+#define DHCP_CLIENTNAME         "ESP-LCD"
+#define SSID                    ""
+#define PASSWORD                ""
+
+#define SERVER_HOST_NAME        "192.168.1.2"
+#define TCP_PORT                27007
+
+#define NTP_UPDATE_INTERVAL_SEC 5*3600
+#define NTP_SERVERS             "pool.ntp.org"
+#define UTC_OFFSET              +1
+#define TIMEZONE                +1
+
+#define LAT                     0.0
+#define LON                     0.0
